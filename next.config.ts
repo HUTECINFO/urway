7584fd460307416a3a67901dc314d6e7b9b63f81
hutecof <hutecinterno@outlook.com>
2026-08-24 08:09:12 -0600

    Allow Travelpayouts Drive CSP connections

--- next.config.ts
+++ next.config.ts
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: https: blob:",
-              "connect-src 'self' https://*.supabase.co https://us.i.posthog.com https://api.travelpayouts.com",
+              "connect-src 'self' https://*.supabase.co https://us.i.posthog.com https://api.travelpayouts.com https://emrldco.com",
               "frame-ancestors 'none'",
               "base-uri 'self'",
               "form-action 'self' https://www.aviasales.com https://www.google.com",
